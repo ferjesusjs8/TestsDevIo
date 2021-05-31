7913b0856b9e56314b42a23f34858dea509276d3
fernando.js8 <fernando.js8@outlook.com>
2021-05-31 18:26:37 -0300

    start

---
+++ TesteDeSoftwareDevIo/TestesDeUnidade/Demo/Calculadora.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo
+{
+    public class Calculadora
+    {
+        public double Somar(double v1, double v2) =>
+            v1 + v2;
+
+        public double Dividir(double v1, double v2) =>
+            v1 / v2;
+    }
+}
