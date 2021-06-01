e030faf9d98cc87d8d2a9dc8590523a775415fda
fernando.js8 <fernando.js8@outlook.com>
2021-06-01 00:16:52 -0300

    Traits

---
+++ TesteDeSoftwareDevIo/Features.Tests/01 - Traits/ClienteTests.cs
@@ -0,0 +1,52 @@
+﻿using Features.Clientes;
+using System;
+using Xunit;
+
+namespace Features.Tests
+{
+    public class ClienteTests
+    {
+        [Fact(DisplayName = "Novo Cliente Válido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            // Arrange
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "Fernando",
+                "Santos",
+                DateTime.Now.AddYears(-31),
+                "fernando@fer.com",
+                true,
+                DateTime.Now);
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.True(result);
+            Assert.Empty(cliente.ValidationResult.Errors);
+        }
+
+        [Fact(DisplayName = "Novo Cliente Inválido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarInvalido()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now,
+                "fernando@fer.com",
+                true,
+                DateTime.Now);
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.False(result);
+            Assert.NotEmpty(cliente.ValidationResult.Errors);
+        }
+    }
+}
